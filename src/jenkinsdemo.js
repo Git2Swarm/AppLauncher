793c3837143cf28f3910f3144f303dc8c6b27a9e
Ubuntu <ubuntu@ip-172-31-11-8.us-west-1.compute.internal>
2017-04-04 15:12:01 +0000

    support for stack and job list

--- src/jenkinsdemo.js
+++ src/jenkinsdemo.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var Client = require('node-rest-client').Client;
-var app = express();
 var StringDecoder = require('string_decoder').StringDecoder;
+var _ = require('lodash');
 
+var app = express();
 var fs = require("fs");
 var url = require('url');
+var stackClient = new Client();
 
 if ( process.env.JENKINS_URL == undefined ||
      process.env.JENKINS_USER == undefined ||
      process.env.JENKINS_PASSWORD == undefined ||
+     process.env.STACK_LIST_URL == undefined ||
      process.env.ARTIFACTORY_URL == undefined ||
      process.env.ARTIFACTORY_USER == undefined ||
      process.env.ARTIFACTORY_PASSWORD == undefined ||
      process.env.DOCKER_HUB_USER == undefined ||
      process.env.DOCKER_HUB_PASSWORD == undefined ) {
 
-     console.log("ERROR: set enviroment variable JENKINS_URL, JENKINS_USER, JENKINS_PASSWORD, ARTIFACTORY_URL, ARTIFACTORY_USER, ARTIFACTORY_PASSWORD, DOCKER_HUB_USER, and DOCKER_HUB_PASSWORD");
+     console.log("ERROR: set enviroment variable JENKINS_URL, JENKINS_USER, JENKINS_PASSWORD, STACK_LIST_URL, ARTIFACTORY_URL, ARTIFACTORY_USER, ARTIFACTORY_PASSWORD, DOCKER_HUB_USER, and DOCKER_HUB_PASSWORD");
      process.exit(1);
 }
 
 var jenkinsURL = process.env.JENKINS_URL;
 var jenkinsUser = process.env.JENKINS_USER;
 var jenkinsPassword = process.env.JENKINS_PASSWORD;
+
+var stackListUrl = process.env.STACK_LIST_URL;
 
 var options_auth = { user: jenkinsUser, password: jenkinsPassword };
 var client = new Client(options_auth);
@@ -30,6 +35,7 @@ process.on('uncaughtException', function(err) {
     // handle the error safely
     console.log(err)
 });
+
 
 app.get('/', function (request, response){
     fs.readFile('jenkinsdemo.html', function (err, html) {
@@ -117,6 +123,8 @@ DOCKER_HUB_PASSWORD=${process.env.DOCKER_HUB_PASSWORD}</propertiesContent>
         }).on('error', function(e) {
             console.log("Error while reading container jenkinsURL", e);
         });
+
+
     }).on('error', function(e) {
         console.log("Error while reading container jenkinsURL", e);
     });
@@ -139,8 +147,8 @@ app.get('/startJenkinsBuild', function(request, response) {
         };
 
         client.post(jenkinsURL + "/job/" + query.jobName + "/build", args, function (data, response) {
-	         console.log("Build Data");
-           console.log(data);
+            console.log("Build Data");
+            console.log(data);
 
             var decoder = new  StringDecoder('utf8');
             console.log(decoder.write(data));
@@ -149,8 +157,38 @@ app.get('/startJenkinsBuild', function(request, response) {
     }).on('error', function(e) {
         console.log("Error while reading container jenkinsURL", e);
     });
-	  response.end();
+    response.end();
 });
+
+app.get('/deleteJob', function(request, response) {
+    var query = url.parse(request.url, true).query;
+    var gitHubURL = query.gitHubURL;
+    console.log("IN deleteJob");
+    console.log(query);
+
+    client.get(jenkinsURL + "/crumbIssuer/api/xml", function (data, response) {
+        console.log("Crumb Issuer");
+        console.log(data);
+        var crumbStr = data.defaultCrumbIssuer.crumb;
+
+        var args = {
+            headers: { "Content-Type": "application/xml", "Jenkins-Crumb" : crumbStr }
+        };
+
+        client.post(jenkinsURL + "/job/" + query.jobName + "/doDelete/buildWithParameters?token=deletetoken", args, function (data, response) {
+            console.log("Delete Job");
+            console.log(data);
+
+            var decoder = new  StringDecoder('utf8');
+            console.log(decoder.write(data));
+        });
+
+    }).on('error', function(e) {
+        console.log("Error while reading container jenkinsURL", e);
+    });
+        response.end();
+});
+
 
 app.get('/deleteStack', function(request, response) {
     var query = url.parse(request.url, true).query;
@@ -214,48 +252,28 @@ function refreshStack(){
 } refreshStack(setInterval(refreshStack, 7000))
 
 app.get('/listStack', function (request, response) {
-    var buf = new Buffer(1024);
-    fs.open('list.txt', 'r',function (err,fd) {
-        if (err) throw err;
-        fs.read(fd, buf, 0, buf.length, 0, function(err, bytes) {
-            if(err) throw err;
-            if(bytes>0) {
-            var str=((buf.slice(0, bytes).toString()));
-            str = str.replace(/\n/g, "@");
-            str =str.match("=================(.*)================");
-            str=str[0];
-            str = str.match("=================(.*)=================");
-            str=str[1];
-            str=str.split("@");
-            var row, cell;
-            var htmlTable = "<table id ='table'>";
-            for(var i=1; i < str.length -1; i++) {
-                var strn = str[i];
-                var sname =strn.substring((0),strn.indexOf(" "));
-                for(var j=i; j<=i; j++){
-                    var description=strn.substring(strn.indexOf(" ")+1);
-                    for (row = i; row <=i; row++) {
-                        htmlTable +="<tr id='trid"+ row+"'>";
-                        for (cell = 0; cell <3 ; cell++) {
-                            if (cell == 0) {
-                                htmlTable += "<td id = 'tdid" + row + cell + "'>" +"<button style = 'height:30px; background-color:LightSteelBlue; cursor:pointer; color:white; border:1px solid black;' onclick = 'deleteListStack(this.id)' id = 'btid" + row + "'>"+ "Stop Stack" + "</button>" + "</td>";
-                            } else if(cell == 1) {
-                                htmlTable += "<td style = 'padding-left:4%' id ='tdid" + row + cell + "'>" + sname + "</td>";
-                            } else {
-                                htmlTable +="<td style = 'padding-left:9%' id = 'tdid" + row + cell + "'>" + description + "</td>";
-                            }
-                        }
-                        htmlTable +="</tr>"
-                    }
-                }
-            }
-            htmlTable += "</table>";
-            response.writeHeader(200, {"Content-Type": "text/html"});
-            response.write(htmlTable);
-	          response.end();
-            }
-        });
+    var jsondata;
+    stackClient.get(stackListUrl, function ( data, res ) {
+        jsondata = JSON.parse(data);
+        response.writeHeader(200, {"Content-Type": "apllication/json"});
+        response.write(JSON.stringify(jsondata));
+        response.end();
     });
+});
+
+app.get ('/jobList', function (req, response) {
+    client.get(jenkinsURL + "/api/json", function (data, res) {
+        x = data.jobs;
+        var y = _.filter(x, {"_class":"org.jenkinsci.plugins.workflow.job.WorkflowJob"})
+        output = [];
+        for (var key in y) {
+            output.push({Class:y[key]._class, Name:y[key].name, Color:y[key].color});
+        }
+        response.writeHeader(200, {"Content-Type" : "application/json"});
+        response.write(JSON.stringify(output));
+        response.end();
+    });
+
 });
 
 var server = app.listen(5000, function () {
